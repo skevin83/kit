d915a45e9f2e67bf225f74a4f706cd84a65ed114
Kevin See <kevin.see@elevat.tech>
2021-01-11 09:01:23 +0000

    Added Docker install script

---
+++ install-docker/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo yum update -y && sudo yum upgrade -y
+sudo yum install -y git
+sudo yum install -y docker || sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker centos
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo ""
+echo ""
+echo ">>>>>>>>>>>> Installation is completed! <<<<<<<<<<<<<"
+echo ""
+echo ""
+docker --version
+docker-compose --version
