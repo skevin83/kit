c364eab0e9efea10ed7f2addd694d44e43a6373d
Kevin See <kevin.see@elevat.tech>
2021-02-22 22:32:53 +0800

    Update eks-tools

--- eks-tools/setup.sh
+++ eks-tools/setup.sh
@@ -4,6 +4,14 @@ sudo curl --silent --location -o /usr/local/bin/kubectl \
 
 sudo chmod +x /usr/local/bin/kubectl
 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
 sudo pip install --upgrade awscli && hash -r
 
 sudo yum -y install jq gettext bash-completion moreutils
