6bf2e7d464177b6e16ffa961d209c46c9cb528cf
Kevin See <kevin.see@elevat.tech>
2021-03-22 17:54:20 +0800

    Update script

--- install-chrome/amazonlinux/app.sh
+++ install-chrome/amazonlinux/app.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-sudo cat >/etc/yum.repos.d/google-chrome.repo <<EOL
+cat >/etc/yum.repos.d/google-chrome.repo <<EOL
 [google-chrome]
 name=google-chrome
 baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
@@ -7,4 +7,4 @@ enabled=1
 gpgcheck=1
 gpgkey=https://dl.google.com/linux/linux_signing_key.pub
 EOL
-sudo yum update -y && sudo yum install google-chrome-stable -y
+yum update -y && yum install google-chrome-stable -y
---
+++ setup-encoding/amazonlinux/app.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+cat >/etc/environment <<EOL
+LANG=en_US.utf-8
+LC_ALL=en_US.utf-8
+EOL
