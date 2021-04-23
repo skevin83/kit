f1930426aa56c05d6bef2023b6c5ba178496696b
Kevin See <kevin.see@elevat.tech>
2021-04-23 11:30:45 +0800

    Update kubectl

--- eks-tools/setup.sh
+++ eks-tools/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo curl --silent --location -o /usr/local/bin/kubectl \
-   https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.11/2020-09-18/bin/linux/amd64/kubectl
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
 
 sudo chmod +x /usr/local/bin/kubectl
 
