9fc93bfb4bb5ba7bbdbebc1588dcd041c4203ad8
Kevin See <kevin.see@elevat.tech>
2021-06-09 16:15:09 +0800

    Added Docker setup script for Ubuntu variant

---
+++ install-docker/ubuntu/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl start docker
+sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo ""
+echo ""
+echo ">>>>>>>>>>>> Installation is completed! <<<<<<<<<<<<<"
+echo ""
+echo ""
+docker --version
+docker-compose --version
