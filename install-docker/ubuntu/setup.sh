879304f4aa918f018e3dcb582d2ca23753e0ab1c
kevin.see <kevin.see@elevat.tech>
2021-08-09 15:40:00 +0800

    Update docker.sock permission

--- install-docker/ubuntu/setup.sh
+++ install-docker/ubuntu/setup.sh
@@ -7,6 +7,7 @@ apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo systemctl start docker
 sudo usermod -aG docker ${USER}
+sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 echo ""
