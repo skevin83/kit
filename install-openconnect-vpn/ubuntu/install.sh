0a51a791fad2bdecec57c8dea4de9fd7624475c8
kevin.see <kevin.see@elevat.tech>
2021-08-17 08:25:22 +0800

    Added openconnect-vpn

---
+++ install-openconnect-vpn/ubuntu/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install -y openconnect network-manager-openconnect network-manager-openconnect-gnome
+sudo apt install -y openvpn network-manager-openvpn network-manager-openvpn-gnome
