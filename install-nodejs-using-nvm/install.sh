8cd1ff69718b98a9a36875bcf4708bb9d2c6e5fd
kevin.see <kevin.see@elevat.tech>
2021-08-30 17:48:39 +0800

    Added install NodeJS

---
+++ install-nodejs-using-nvm/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install v16.6.1 #change your node version here
